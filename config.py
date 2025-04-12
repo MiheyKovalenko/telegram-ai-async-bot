51ba9693f7842399270d73d7d383caac211bd35b
Михей <40692916+MiheyKovalenko@users.noreply.github.com>
2025-04-12 20:46:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = config['default']['bot_token']
 ADMIN_ID = int(config['default']['admin_id'])
 OPENAI_API_KEY = config['default']['openai_api_key']
 
-PAID_USERS = [435230225]  # юзеры с доступом к GPT-4o
+PAID_USERS = []  # юзеры с доступом к GPT-4o
